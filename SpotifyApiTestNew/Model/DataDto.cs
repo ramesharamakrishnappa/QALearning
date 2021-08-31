456238e99eea1cbbca4f60a9048fd4810b3f6f4f
ramesharamakrishnappa <84889685+ramesharamakrishnappa@users.noreply.github.com>
2021-08-31 13:51:30 +0530

    Update DataDto.cs

--- SpotifyApiTestNew/Model/DataDto.cs
+++ SpotifyApiTestNew/Model/DataDto.cs
@@ -12,7 +12,5 @@ namespace SpotifyApiTest.Model
         public string type { get; set; }
         public string uri { get; set; }
         public string name { get; set; }
-        public string name2 { get; set; }
-        public string name3 { get; set; }
-    }
+            }
 }
